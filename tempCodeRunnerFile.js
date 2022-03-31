d5b7e3bceec64ea9b6a960ac488a6a885088ba81
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-03-30 16:26:32 -1000

    first commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+getUserById(users, '16t', user => {
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address) 
+})
